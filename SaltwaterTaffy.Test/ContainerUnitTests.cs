f772b5872efb41c3364b24d029aa98fa817265e9
Thom Dixon <thom@thomdixon.org>
2013-08-20 17:54:33 -0400

    Rewrote nmaprun object parsing
    Rewrote the constructor of the ScanResult object's nmaprun parsing.
    Also added some unit tests for the Target object's constructor, and
    added documentation comments.

---
+++ SaltwaterTaffy.Test/ContainerUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaltwaterTaffy.Container;
+
+namespace SaltwaterTaffy.Test
+{
+    [TestClass]
+    public class ContainerUnitTests
+    {
+        [TestMethod]
+        public void when_Target_constructed_with_strings_then_ToString_should_yield_space_separated_target()
+        {
+            var t = new Target(new[] {"this", "is", "a", "test"});
+            Assert.AreEqual("this is a test", t.ToString());
+        }
+
+        [TestMethod]
+        public void when_Target_constructed_with_string_then_ToString_should_yield_target()
+        {
+            var t = new Target("this is a test");
+            Assert.AreEqual("this is a test", t.ToString());
+        }
+
+        [TestMethod]
+        public void when_Target_constructed_with_IPAddress_then_ToString_should_yield_target()
+        {
+            var t = new Target(IPAddress.Parse("127.0.0.1"));
+            Assert.AreEqual("127.0.0.1", t.ToString());
+        }
+
+        [TestMethod]
+        public void when_Target_constructed_with_IPAddresses_then_ToString_should_yield_space_separated_target()
+        {
+            var t = new Target(new[] { IPAddress.Parse("127.0.0.1"), IPAddress.Parse("127.0.0.2"), IPAddress.Parse("127.0.0.3") });
+            Assert.AreEqual("127.0.0.1 127.0.0.2 127.0.0.3", t.ToString());
+        }
+    }
+}

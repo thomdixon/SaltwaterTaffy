a8c6d3f8c232c0d176684d5cca7f6e9a91e24f30
Thom Dixon <thom@thomdixon.org>
2013-08-23 09:26:00 -0400

    Added simple demo project
    Wrote a very simple CLI utility that will scan a subnet or specific
    target and report back its findings in order to demonstrate how the
    library may be used

---
+++ SaltwaterTaffy.Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SaltwaterTaffy.Container;
+
+namespace SaltwaterTaffy.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an IP or subnet: ");
+            var target = new Target(Console.ReadLine().Trim());
+            Console.WriteLine("Initializing scan of {0}", target);
+            var result = new Scanner(target).PortScan();
+            Console.WriteLine("Detected {0} host(s), {1} up and {2} down.", result.Total, result.Up, result.Down);
+            foreach (var i in result.Hosts)
+            {
+                Console.WriteLine("Host: {0}", i.Address);
+                foreach (var j in i.Ports)
+                {
+                    Console.Write("\tport {0}", j.PortNumber);
+                    if (!string.IsNullOrEmpty(j.Service.Name))
+                    {
+                        Console.Write(" is running {0}", j.Service.Name);
+                    }
+
+                    if (j.Filtered)
+                    {
+                        Console.Write(" is filtered");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                if (i.OsMatches.Any())
+                {
+                    Console.WriteLine("and is probably running {0}", i.OsMatches.First().Name);   
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}

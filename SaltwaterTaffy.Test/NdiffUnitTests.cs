6e0ed979cf83d5df5be0d686a32aaedf42a22ec6
Justin <jmooney5115@gmail.com>
2017-12-05 13:38:03 -0600

    Add Ndiff Wrapper
    Hi Thom, another pull request for you. I added an Ndiff wrapper
    
    I included 2 XML Nmap output files for testing. Honestly, I don't know where test files should go so I placed them in a folder inside bin/debug folder in the test project. Let me know how you want it and I will modify to fit.
    
    I'm using VS2017 so I don't know if the commited .csproj files will cause any issue.

---
+++ SaltwaterTaffy.Test/NdiffUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SaltwaterTaffy.Test
+{
+    [TestClass]
+    public class NdiffUnitTests
+    {
+
+        public static string TestFileDir = Environment.CurrentDirectory + "\\TestFiles";
+
+        [TestMethod]
+        public void diff_two_nmap_output_files_good_files()
+        {
+            var file1 = Path.Combine(TestFileDir, "scanme.nmap.org.xml");
+            var file2 = Path.Combine(TestFileDir, "scanme.nmap.org_modified.xml");
+
+            var verbose = false;
+            var xml = false;
+
+            var output = Ndiff.RunDiff(verbose, xml, file1, file2);
+
+
+            Assert.AreEqual(output, "\r\n scanme.nmap.org (45.33.32.156):\r\n PORT   STATE SERVICE VERSION\r\n-22/tcp open  ssh     OpenSSH 6.6.1p1 Ubuntu 2ubuntu2.8 (Ubuntu Linux; protocol 2.0)\r\n");
+        }
+
+        [TestMethod]
+        public void diff_two_nmap_output_files_non_existent_files()
+        {
+            var file1 = Path.Combine(TestFileDir, "im_not_here.xml");
+            var file2 = Path.Combine(TestFileDir, "me_neither.xml");
+
+            var verbose = false;
+            var xml = false;
+
+            var output = Ndiff.RunDiff(verbose, xml, file1, file2);
+
+            var fail = output.Contains("Can't open file: ") ? true : false;
+
+            Assert.IsTrue(fail);
+        }
+    }
+}
---
+++ SaltwaterTaffy/Ndiff.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SaltwaterTaffy
+{
+    public static class Ndiff
+    {
+        /// <summary>
+        ///     Run the Ndiff tool to compare output files from Nmap.
+        /// </summary>
+        /// <param name="file1">Original File</param>
+        /// <param name="file2">Modified File</param>
+        /// <param name="verbose">Also show hosts and ports that haven't changed.</param>
+        /// <param name="xml">Display output in XML format.</param>
+        /// <returns>Output from Ndiff application.</returns>
+        public static string RunDiff(bool verbose, bool xml, string file1, string file2)
+        {
+            try
+            {
+                string output, error;
+                var nmap = new NmapContext();
+
+                var sVerbose = verbose == true ? "-v" : "";
+                var sXml = xml == true ? "--xml" : "--text";
+
+                using (var process = new Process())
+                {
+                    //thanks for the code T30 @ stackoverflow
+                    //https://stackoverflow.com/questions/4291912/process-start-how-to-get-the-output
+                    process.StartInfo.FileName = nmap.GetPathToNdiff();
+                    process.StartInfo.Arguments = string.Format("{0} {1} {2} {3}", sVerbose, sXml, file1, file2);
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.Start();
+
+                    //* Read the output (or the error)
+                    output = process.StandardOutput.ReadToEnd();
+                    error = process.StandardError.ReadToEnd();
+
+                    process.WaitForExit();
+                }
+
+
+                return string.IsNullOrEmpty(error) ? output : error;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Failed to run diff. Ex: {0}", ex.ToString()));
+            }
+        }
+    }
+}
